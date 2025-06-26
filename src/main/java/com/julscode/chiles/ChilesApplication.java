5d23316286d213f46011ed6e3663a278b3a1ebe5
julscode <jcah549@gmail.com>
2025-06-26 08:58:09 -0600

    segundo commit

---
+++ src/main/java/com/julscode/chiles/ChilesApplication.java
@@ -0,0 +1,13 @@
+package com.julscode.chiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/julscode/chiles/ChilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julscode.chiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
